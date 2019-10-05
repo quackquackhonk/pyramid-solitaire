c21c54b7f0f301ed2df5c7885ec4ccd193283e31
Varun Tankala <tankalavarun@gmail.com>
2019-10-04 20:39:36 -0400

    Changed Card implementation to use an interface rather than the Suit.Empty implementation in prior version. Added tests for startGame invalid rows and numDraw error handling.

---
+++ src/cs3500/pyramidsolitaire/model/hw02/EmptyCard.java
@@ -0,0 +1,54 @@
+package cs3500.pyramidsolitaire.model.hw02;
+
+/**
+ * Represents a Playing Card that has been played and discarded in a game of Pyramid Solitaire.
+ * This is a placeholder value to prevent unwanted NullPointerExceptions when accessing the deck
+ * and draw pile.
+ */
+public class EmptyCard implements Card {
+
+
+  @Override
+  public CardValue getValue() {
+    return null;
+  }
+
+  @Override
+  public int getIntegerValue() {
+    return 0;
+  }
+
+  @Override
+  public Suit getSuit() {
+    return null;
+  }
+
+  @Override
+  public boolean getVisibility() {
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "  ";
+  }
+
+  @Override
+  public boolean equals(Object o) {
+
+    if (o == this) {
+      return true;
+    }
+
+    if (!(o instanceof EmptyCard)) {
+      return false;
+    }
+    // all empty cards are the same
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return 0;
+  }
+}
